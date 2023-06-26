df45bb8e4d837900b0d324937c811eb336a0ab28
Peridendes <peridendes@reacher.me>
2023-06-26 13:52:45 -0400

    changed from rows to columns

--- mempool.py
+++ mempool.py
@@ -15,7 +15,7 @@ def get_mempool_data():
             response = requests.get(url)
             response.raise_for_status()
             data = response.json()
-            blocks = data[:7] # 7 row limitation of UnicornHATmini
+            blocks = data[:8] # Retrieve 8 blocks from the API
             return blocks
         except (requests.exceptions.RequestException, ValueError) as e:
             print(f"Error occurred: {e}")
@@ -31,11 +31,7 @@ def calculate_segment_colors(fee_range):
     segment_colors = []
 
     for fee in fee_range:
-        if fee == 1:
-            r = 255
-            g = 255
-            b = 255
-        elif fee <= 5:
+        if fee <= 5:
             # Gradient from white to blue
             r = int(255 * (5 - fee) / 4)
             g = int(255 * (5 - fee) / 4)
@@ -73,25 +69,29 @@ def calculate_segment_colors(fee_range):
 # Function to convert mempool data to LED pixels
 def convert_data_to_led_pixels(blocks):
     led_pixels = []
+
+    blocks = blocks[::-1]  # Reverse the order of blocks
+
     for block in blocks:
         block_size = block['blockSize']
-        row_length = min(math.ceil(block_size / (2 * 1024 * 1024) * 17), 17)  # Calculate row length
+        column_length = min(math.ceil(block_size / (2 * 1024 * 1024) * 7), 7)  # Calculate column length
         fee_range = block['feeRange']
 
         segment_colors = calculate_segment_colors(fee_range)
-
-        led_row = []
-        segment_lengths = [row_length // 7] * 7
-        remainder = row_length % 7
+        segment_colors.reverse()
+        
+        led_col = []
+        segment_lengths = [column_length // 7] * 7
+        remainder = column_length % 7
 
         for i in range(remainder):
             segment_lengths[i] += 1
 
-        for i in range(7):
-            led_row.extend([segment_colors[i]] * segment_lengths[i])
+        led_col.extend([(0, 0, 0)] * (7 - column_length))
+        for i in range(len(segment_colors)):
+            led_col.extend([segment_colors[i]] * segment_lengths[i])
 
-        led_row.extend([(0, 0, 0)] * (17 - row_length))
-        led_pixels.append(led_row)
+        led_pixels.append(led_col)
 
     return led_pixels
 
@@ -105,7 +105,7 @@ while True:
     for y, led_row in enumerate(led_pixels):
         for x, pixel_color in enumerate(led_row):
             r, g, b = pixel_color
-            unicornhatmini.set_pixel(x, y, r, g, b)
+            unicornhatmini.set_pixel(y, x, r, g, b)
 
     unicornhatmini.show()
     time.sleep(15)  # Wait for 15 seconds before refreshing the data and screen
