76eae71e097e20c1f7724aa33c3b5d8662007ad8
Peridendes <peridendes@reacher.me>
2023-06-26 11:45:35 -0400

    fix colors

--- mempool.py
+++ mempool.py
@@ -1,29 +1,70 @@
 import requests
+import math
 import time
 from unicornhatmini import UnicornHATMini
 
 # Function to retrieve mempool data from the API
 def get_mempool_data():
-    response = requests.get('https://mempool.space/api/v1/fees/mempool-blocks')
-    data = response.json()
-    blocks = data[:7]  # Consider only the first 7 blocks
-    return blocks
+    url = 'https://mempool.space/api/v1/fees/mempool-blocks'
+    max_retries = 3
+    retry_interval = 15  # seconds
+    retries = 0
+
+    while retries < max_retries:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            blocks = data[:7] # 7 row limitation of UnicornHATmini
+            return blocks
+        except (requests.exceptions.RequestException, ValueError) as e:
+            print(f"Error occurred: {e}")
+            print("Retrying after 15 seconds...")
+            time.sleep(retry_interval)
+            retries += 1
+
+    print("Max retries exceeded. Exiting...")
+    return None
 
 # Function to calculate the segment colors based on fee range
 def calculate_segment_colors(fee_range):
     segment_colors = []
 
     for fee in fee_range:
-        if fee <= 30:
-            # Gradient from green (1) to yellow to orange to red (300)
-            r = int(255 * (fee / 30))
-            g = int(255 * (1 - (fee / 30)))
+        if fee == 1:
+            r = 255
+            g = 255
+            b = 255
+        elif fee <= 5:
+            # Gradient from white to blue
+            r = int(255 * (5 - fee) / 4)
+            g = int(255 * (5 - fee) / 4)
+            b = 255
+        elif fee <= 10:
+            # Gradient from blue to green
+            r = 0
+            g = int(255 * (fee - 5) / 5)
+            b = int(255 * (10 - fee) / 5)
+        elif fee <= 15:
+            # Gradient from green to yellow
+            r = int(255 * (fee - 10) / 5)
+            g = 255
+            b = 0
+        elif fee <= 20:
+            # Gradient from yellow to orange
+            r = 255
+            g = int(255 - (127 * (20 - fee) / 5 ))
+            b = 0
+        elif fee <= 60:
+            # Gradient from orange to red
+            r = 255
+            g = int(128  * (60 - fee) / 40)
             b = 0
         else:
-            # Fuchsia for fees over 300
+            # Gradient from red to fuchsia
             r = 255
             g = 0
-            b = 255
+            b = int(255 * (max(fee_range) - fee) / (max(fee_range) - 60))
 
         segment_colors.append((r, g, b))
 
@@ -31,76 +72,10 @@ def calculate_segment_colors(fee_range):
 
 # Function to convert mempool data to LED pixels
 def convert_data_to_led_pixels(blocks):
-    max_block_size = max(block['blockSize'] for block in blocks)
-
     led_pixels = []
     for block in blocks:
         block_size = block['blockSize']
-        row_length = int(block_size / max_block_size * 17)
-        fee_range = block['feeRange']
-
-        segment_colors = calculate_segment_colors(fee_range)
-
-        led_row = []
-        segment_lengths = [row_length // 7] * 7
-        remainder = row_length % 7
-
-        for i in range(remainder):
-            segment_lengths[i] += 1
-
-        for i in range(7):
-            led_row.extend([segment_colors[i]] * segment_lengths[i])
-
-        led_row.extend([(0, 0, 0)] * (17 - row_length))
-        led_pixels.append(led_row)
-
-    return led_pixels
-
-# Main program
-unicornhatmini = UnicornHATMini()
-
-while True:
-    blocks = get_mempool_data()
-    led_pixels = convert_data_to_led_pixels(blocks)
-import requests
-import time
-from unicornhatmini import UnicornHATMini
-
-# Function to retrieve mempool data from the API
-def get_mempool_data():
-    response = requests.get('https://mempool.space/api/v1/fees/mempool-blocks')
-    data = response.json()
-    blocks = data[:7]  # Consider only the first 7 blocks
-    return blocks
-
-# Function to calculate the segment colors based on fee range
-def calculate_segment_colors(fee_range):
-    segment_colors = []
-
-    for fee in fee_range:
-        if fee <= 30:
-            # Gradient from green (1) to yellow to orange to red (300)
-            r = int(255 * (fee / 30))
-            g = int(255 * (1 - (fee / 30)))
-            b = 0
-        else:
-            # Fuchsia for fees over 300
-            r = 255
-            g = 0
-            b = 255
-
-        segment_colors.append((r, g, b))
-
-    return segment_colors
-
-# Function to convert mempool data to LED pixels
-def convert_data_to_led_pixels(blocks):
-    max_block_size = max(block['blockSize'] for block in blocks)
-
-    led_pixels = []
-    for block in blocks:
-        block_size = block['blockSize']
-        row_length = int(block_size / max_block_size * 17)
+        row_length = min(math.ceil(block_size / (2 * 1024 * 1024) * 17), 17)  # Calculate row length
         fee_range = block['feeRange']
 
         segment_colors = calculate_segment_colors(fee_range)
