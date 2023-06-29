b770e33bffe076dcf735f6add45bbca85949431f
Peridendes <peridendes@reacher.me>
2023-06-29 05:50:14 -0400

    medianFee arraay

--- debug.py
+++ debug.py
@@ -163,10 +163,10 @@ def convert_block_data_to_led_pixels(blocks):
 
     for block in blocks:
         bar_length = calculate_bar_length(block['size'])
-        medianFee = block['extras']['medianFee']
-        fee = int(medianFee)
+        medianFee = []
+        medianFee.append(block['extras']['medianFee'])
 
-        led_color = fee_colors(fee)
+        led_color = fee_colors(medianFee)
 
         # Create a column of LED pixels with the same color       
         led_bar = [led_color] * bar_length
