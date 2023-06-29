210d94e5c9aab84b484d55193c0bc4f735310007
Peridendes <peridendes@reacher.me>
2023-06-29 06:52:40 -0400

    adjust block color gradients

--- debug.py
+++ debug.py
@@ -129,10 +129,10 @@ def rgb_fees(fee, data_type):
             g = int(255 * (max(fee) - fee) / (max(fee) - 60))
             b = int(255 * (max(fee) - fee) / (max(fee) - 60))
     else:
-        # Blue to Purple 
+        # Blue to Red 
         r = int(255 * min((fee / 60), 1))
         g = 0
-        b = 255
+        b = int(255 * min ((60 - fee)/60), 0)
 
     return r, g, b
 
